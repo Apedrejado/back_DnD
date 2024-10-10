dd5aa7c31caec29a8673833fcc295f04066a1fe8
Apedrejado <giovanigsilvano@gmail.com>
2024-10-10 20:55:14 -0300

    feat: calculator and races bonus fix

--- src/main/kotlin/character/Character.kt
+++ src/main/kotlin/character/Character.kt
@@ -3,25 +3,25 @@ package main.kotlin.character
 import character.Attributes
 import org.example.classes.CharacterClass
 import org.example.races.RaceStrategy
-import org.example.utils.Calculator
+    import org.example.utils.Calculator
 
 
-class Character(
-    val name: String,
-    val race: RaceStrategy,
-    val characterClass: CharacterClass,
-    val alignment: String,
-    val background: String,
-    baseAttributes: Attributes,
-) {
-    private var _baseAttributes: Attributes = baseAttributes
-    private var _adjustedAttributes: Attributes = race.applyRaceBonuses(_baseAttributes)
-    private val calculator = Calculator()
+    class Character(
+        val name: String,
+        val race: RaceStrategy,
+        val characterClass: CharacterClass,
+        val alignment: String,
+        val background: String,
+        baseAttributes: Attributes,
+    ) {
+        private var _baseAttributes: Attributes = baseAttributes
+        private var _adjustedAttributes: Attributes = race.applyRaceBonuses(_baseAttributes)
+        private val calculator = Calculator()
 
-    val baseAttributes: Attributes
-        get() = _baseAttributes
+        val baseAttributes: Attributes
+            get() = _baseAttributes
 
-    val adjustedAttributes: Attributes
-        get() = _adjustedAttributes
+        val adjustedAttributes: Attributes
+            get() = _adjustedAttributes
 
-}
+    }
