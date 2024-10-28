3807d9137a29524207542a53a44112d43cf2b6d9
Apedrejado <giovanigsilvano@gmail.com>
2024-10-28 17:41:01 -0300

    feat:  character add id

--- src/main/kotlin/character/Character.kt
+++ src/main/kotlin/character/Character.kt
@@ -3,17 +3,19 @@ package main.kotlin.character
 import character.Attributes
 import org.example.classes.CharacterClass
 import org.example.races.RaceStrategy
-    import org.example.utils.Calculator
+import org.example.utils.Calculator
 
 
     class Character(
+
+        val id : Int =0,
         val name: String,
         val race: RaceStrategy,
         val characterClass: CharacterClass,
         val alignment: String,
         val background: String,
         baseAttributes: Attributes,
-    ) {
+) {
         private var _baseAttributes: Attributes = baseAttributes
         private var _adjustedAttributes: Attributes = race.applyRaceBonuses(_baseAttributes)
         private val calculator = Calculator()
